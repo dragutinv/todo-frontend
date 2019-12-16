9f1a6e5a2f52b17ec4ac4ed51640ffbec7a5645e
draugtin <dragutin.v@gmail.com>
2019-12-16 20:47:04 +0100

    Remove demo files, and start with todo app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# todo
+# Front end for 'todo' project
 
 ## Project setup
 ```
@@ -19,7 +19,3 @@ npm run build
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-# todo-frontend
---
+++ src/views/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+  <h3>TODO list</h3>
+  <div v-for="item in items" v-bind:key="item.id">
+    {{ item.message }}
+    <ul>
+      <li v-for='item in item.categories' v-bind:key="item.id">
+          {{item.category}}
+      </li>
+    </ul>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'todo',
+  data () {
+    return {
+      items: [
+        { message: 'Do this', categories: [ { category: 'cat1' } ] },
+        { message: 'Do that', categories: [ { category: 'cat2' } ] }
+      ]
+    }
+  },
+  created () {
+  }
+}
+</script>
